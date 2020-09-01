8bdfbf07c8152bc5b2bd384fb7971a189d0fa775
marumaruda <56784724+marumaruda@users.noreply.github.com>
2020-09-01 15:54:10 +0900

    Add files via upload

---
+++ index.js
@@ -0,0 +1,19 @@
+var _window = $(window),
+    _header = $('.header'),
+    heroBottom;
+ 
+_window.on('scroll',function(){
+    heroBottom = $('.header').height();
+    if(_window.scrollTop() > heroBottom){
+        _header.addClass('transform');   
+    }
+    else{
+        _header.removeClass('transform');   
+    }
+});
+ 
+_window.trigger('scroll');
+
+$(function(){
+    $('.title').addClass('opa')
+});
